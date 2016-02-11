96f2b0c90522b8625a301f11590baecf282d001f
phgrey <phgrey@readdle.com>
2016-02-11 08:18:20 +0200

    some more examples

---
+++ src/echo.erl
@@ -0,0 +1,22 @@
+
+-module(echo).
+-export([go/0, loop/0]).
+-record(cat, {name, weight, hello}).
+
+go() ->
+  Pid2 = spawn(echo, loop, []),
+  Pid2 ! {self(), hello},
+  receive
+    {Pid2, Msg} ->
+      io:format("P1 ~w~n",[Msg])
+  end,
+  Pid2 ! stop.
+
+loop() ->
+  receive
+    {From, Msg} ->
+      From ! {self(), Msg},
+      loop();
+    stop ->
+      true
+  end.
